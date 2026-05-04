78a2a265ec1fac368347d3e5ae187bbc8ae00d45
Terry <danyel.martel@gmail.com>
2026-05-04 08:15:38 -0500

    video added, if dont like, back to now error to update is showing

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,11 +1,11 @@
 import { memo, useCallback } from 'react'
+import woodsVideo from '../../assets/woods.webm'
 
 const HeroSection = memo(() => {
   const handleScrollToTable = useCallback(() => {
     const element = document.getElementById('dkp')
     if (element) {
-      // Calculamos la posición del elemento relativa al documento
-      const yOffset = -80 // El margen que quieres dejar
+      const yOffset = -80
       const y =
         element.getBoundingClientRect().top + window.pageYOffset + yOffset
 
@@ -16,12 +16,20 @@ const HeroSection = memo(() => {
   return (
     <div className='mb-8 @container'>
       <div
-        className='@[480px]:p-0 bg-center bg-no-repeat'
+        className='relative overflow-hidden @container'
         style={{
-          backgroundImage: `linear-gradient(rgba(2, 6, 23, 0.6) 0%, rgba(2, 6, 23, 0.8) 100%), url("https://cdn.discordapp.com/attachments/1260397382588436571/1499277053738877018/cbb96ebd-c815-4601-bc3f-4d31f9b6559a_removalai_preview.png?ex=69f6d910&is=69f58790&hm=6c41a17b27c1cc803fcbb307d89c9e9eb8cc35a47a9afb2caa4f400ab4e024de&")`
+          backgroundImage: `linear-gradient(rgba(2, 6, 23, 0.6) 0%, rgba(2, 6, 23, 0.8) 100%)`
         }}
       >
-        <div className='flex min-h-100 flex-col gap-6 bg-cover bg-center bg-no-repeat @[480px]:gap-8 @[480px]:rounded-xl items-center justify-center p-4 backdrop-blur-sm border border-white/5'>
+        <video
+          src={woodsVideo}
+          autoPlay
+          loop
+          muted
+          playsInline
+          className='absolute inset-0 w-full h-full object-cover z-0'
+        />
+        <div className='relative z-10 flex min-h-100 flex-col gap-6 bg-cover bg-center bg-no-repeat @[480px]:gap-8 @[480px]:rounded-xl items-center justify-center p-4 border border-white/5'>
           <div className='flex flex-col gap-2 text-center'>
             <h1 className='text-4xl font-black leading-tight tracking-[-0.033em] text-white @[480px]:text-5xl @[480px]:font-black @[480px]:leading-tight @[480px]:tracking-[-0.033em]'>
               Bienvenido a Lunatik
